3b810b7acd40b833c3aa0bb7e7fca99fa5435add
AartiDixit <115448333+AartiDixit@users.noreply.github.com>
2023-01-13 03:51:06 -0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const App = () => {
+  const relativeList = ["tejas", "manjiri", "chinmay", "kajal", "shital", "rupali", "sachin"]
+
   return (
     <div id="main">
                {/* Do not remove the main div */}
+<ol>
+        {
+          relativeList.map((relativeListItem1,index)  => 
+            <li key={relativeListItem1}>{relativeListItem1}</li>
+          )
+        }
+      </ol>
+
     </div>
   )
 }
